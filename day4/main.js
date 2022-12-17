75263822976871ea71d3a2068d521646d58038b1
nrychagov <nikita.rychagov@noveogroup.com>
2022-12-17 03:52:37 +0100

    feat: day 4

---
+++ day4/main.js
@@ -0,0 +1,27 @@
+const { readFileSync } = require("fs");
+
+function countTotalPairOverlaps() {
+  const contents = readFileSync("input.txt", "utf-8").split("\n");
+
+  let overlapCounter = 0;
+
+  for (let i = 0; i < contents.length; i++) {
+    const pair = contents[i].split(",").map((el) => el.split("-"));
+    const aRange = pair[0];
+    const bRange = pair[1];
+
+    const range = aRange.concat(bRange).map((n) => parseInt(n));
+
+    const [startA, endA, startB, endB] = range;
+
+    if (
+      (startA <= startB && endA >= endB) ||
+      (startB <= startA && endB >= endA)
+    ) {
+      overlapCounter++;
+    }
+  }
+  return overlapCounter;
+}
+
+console.log(countTotalPairOverlaps());
